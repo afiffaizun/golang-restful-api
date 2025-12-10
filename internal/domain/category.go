4e28686ec02d9846dab2ea1c205940188a399333
afiffaizun <afiffaizun31@gmail>
2025-12-11 06:35:49 +0700

    update category domain model and repository interface

---
+++ internal/domain/category.go
@@ -0,0 +1,37 @@
+package domain
+
+import (
+	"time"
+	"context"
+)
+
+type Category struct {
+	Id  	int64    `json:"id"`
+	Name 	string   `json:"name"`
+	CreateAt time.Time `json:"create_at"`
+	UpdateAt time.Time `json:"update_at"`
+}
+
+type CategoryCreateRequest struct {
+	Name string `json:"name" validate:"required, min=3, max=100"`
+}
+
+type CategoryUpdateRequest struct {
+	Id   int64 `json:"id" validate:"required"`
+	Name string `json:"name" validate:"required, min=3, max=100"`
+}
+
+type CategoryResponse struct {
+	Id  	int64    `json:"id"`
+	Name    string   `json:"name"`
+	CreateAt time.Time `json:"create_at"`
+	UpdateAt time.Time `json:"update_at"`
+}
+
+type CategoryRepository interface {
+	Create(ctx context.Context, category Category) CategoryRepository
+	Update(ctx context.Context, category Category) CategoryRepository
+	Delete(ctx context.Context, categoryId int64)
+	FindById(ctx context.Context, categoryId int64) (Category, error)
+	FindAll(ctx context.Context) []Category
+}
