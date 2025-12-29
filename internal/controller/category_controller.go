9c96969d33f8695dfae441cc3d309800c8bd2164
afiffaizun <afiffaizun31@gmail>
2025-12-30 06:41:12 +0700

    upload

---
+++ internal/controller/category_controller.go
@@ -0,0 +1,59 @@
+package controller
+
+
+import (
+	"encoding/json"
+	"golang-restful-api/internal/domain"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type CategoryController struct {
+	CategoryService domain.CategoryService
+}
+
+func NewCategoryController(categoryService domain.CategoryService) *CategoryController {
+	return &CategoryController{
+		CategoryService: categoryService,
+	}
+}
+
+func (controller *CategoryController) Create(w http.ResponseWriter, r *http.Request) {
+	var request domain.CategoryCreateRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	helper.PanicIfError(err)
+
+	categoryResponse := controller.CategoryService.Create(r.Context(), request)
+
+
+	webResponse := helper.WebResponse{
+		Code:   http.StatusCreated,
+		Status: "Created",
+		Data:   categoryResponse,
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(webResponse)
+}
+
+func (controller *CategoryController) Update(w http.ResponseWriter, r *http.Request) {
+	var request domain.CategoryUpdateRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	helper.PanicIfError(err)
+
+	categoryId := chi.URLParam(r, "categoryId")
+	id, err := strconv.ParseInt(categoryId, 10, 64)
+	helper.PanicIfError(err)
+
+	request.Id = id
+
+	categoryResponse := controller.CategoryService.Update(r.Context(), request)
+	webResponse := helper.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   categoryResponse,
+	}
+}
